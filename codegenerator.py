b4bd2f82413c0c6c4dad590c35a0e1fe224abf79
Arcan99 <112803191+Arcan99@users.noreply.github.com>
2023-03-18 14:15:05 +0100

    read whithout clear lines & Converter Structure

--- codegenerator.py
+++ codegenerator.py
@@ -1,11 +1,64 @@
 # Generate C-code from Easy C
 
 from codeconverter import Converter
+from filereader import File_reader
 
 class Generator:
+    
+    def __init__(self, file_path: str) -> None:
+        self.reader = File_reader(file_path=file_path)
+        self.converter = Converter()
 
-    def temp(self):
-        print("TODO")
+    def convertFile(self, file_path: str):
+        # wrapper text
+        self.generateWrapperText()
+        # includes
+        # defines
+        self.convertDefines(self)
+        # structs
+        self.convertUserStruct(self)
+        # functions 
+        self.convertFunction(self)
+        # main
+        self.convertMain(self)
+        
+    
+    ### Main Methods ###
+    
+    def generateWrapperText(self):
+        # generate misc text at the top of the document
+        pass
+    
+    def convertDefines(self):
+        # User Definitions
+        pass
+    
+    def convertUserStruct(self):
+        # User Structure
+        pass
+    
+    def convertFunction(self):
+        # define name
+        # body
+        # end
+        pass
+    
+    def convertMain(self):
+        # start
+        # body
+        # end
+        pass
+
+     ### Supporting Methods ###
+     
+    def convertBody(self):
+        #differente types of body
+        pass
+    
+    
+    
+            
+            
         
         
 
--- filereader.py
+++ filereader.py
@@ -1,11 +1,9 @@
 # reads text from file and converts it to llists of tokens
 
-
 class File_reader:
     
     def __init__(self, file_path) -> None:
         self.file = open(file_path, "r")
-        self.text = []
         
     def __del__(self):
         self.file.close()
@@ -14,12 +12,25 @@ class File_reader:
         sentence = self.file.readline()
         if len(sentence) == 0:
             return None
-        self.text.append(sentence)
+        return sentence
+        
+    def getNewTokens(self) -> bool:
+        while True:
+            input = self.readNextLine()
+            if input is None:
+                return None
+            input = input.split()
+            if not input == []:
+                break
+        return input
         
     
-      
+
+    
 if __name__ == "__main__":
     reader = File_reader("test_files/main1.txt")
-    for i in range(6):
-        reader.readNextLine()
-    print(reader.text)
+    tokens = reader.getNewTokens()
+    while tokens is not None:
+        print(tokens)
+        tokens = reader.getNewTokens()
+        
